a37711d4b1d13c0d370bb192aa0ff9ad1dbdf3c2
Kartik Badkhal <kartikbadkhal20@gmail.com>
2026-02-14 11:28:10 +0530

    This is first commit

---
+++ bandnamegen.py
@@ -0,0 +1,9 @@
+# Create a greeting for the program
+# Ask the user for the city they grew up in and store it in a variable
+# Ask the user for the name of a pet and store it in a variable
+# Combine the name of the city and pet to show their brand name
+
+print("hola Amigoo !!!!!!")
+cityname = input("Please Enter your city name")
+petname = input("Please enter your pet name ")
+print("Hello sir ,Your bandname is " +  cityname + petname)
