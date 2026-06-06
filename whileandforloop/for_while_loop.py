c882afe6ae0af29ac0155bb6de9933ee9ab4ec07
Kartik Badkhal <kartikbadkhal20@gmail.com>
2026-06-06 22:57:22 +0530

    lifes_in_weeks

--- whileandforloop/for_while_loop.py
+++ whileandforloop/for_while_loop.py
@@ -1,16 +1,16 @@
-logoReeborg's World
-https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+# logoReeborg's World
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
 
-Hurdle 1 ✓🤖
- World Info 
-Python
- Reeborg's keyboard Additional options
-English
+# Hurdle 1 ✓🤖
+#  World Info 
+# Python
+#  Reeborg's keyboard Additional options
+# English
  
 
-reverse step run step pause stop reload 
- 120/120   
-Python CodelibraryA↑AA↓A
+# reverse step run step pause stop reload 
+#  120/120   
+# Python CodelibraryA↑AA↓A
 def turn_right():
     turn_left()
     turn_left()
@@ -34,13 +34,19 @@ jump()
 jump()
 jump()'''
     
-​
+
 '''for steps in range(6):
     jump()'''
     
-number_of_hurdles = 6
-while number_of_hurdles > 0:
-    jump()
-    number_of_hurdles -= 1
-    print(number_of_hurdles)
+# number_of_hurdles = 6
+# while number_of_hurdles > 0:
+#     jump()
+#     number_of_hurdles -= 1
+#     print(number_of_hurdles)
+# =========================================================
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
 
